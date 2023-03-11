0c85b789c3b72d1261f9a47fe1185bd9d9c84261
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-03-11 17:36:56 -0500

    Adding nvim configuration (WIP)

---
+++ .config/nvim/lua/plugins/rust.lua
@@ -0,0 +1,6 @@
+return {
+  { "neovim/nvim-lspconfig" },
+  { "simrat39/rust-tools.nvim" },
+  { "nvim-lua/plenary.nvim" },
+  { "mfussenegger/nvim-dap" },
+}
---
+++ .config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+      require("onedark").load()
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
