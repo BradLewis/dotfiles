74f108f173df5dc1698c3c8e430f65f72834254f
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-03-13 13:48:13 -0400

    Using clippy with rust analyser

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,4 +1,5 @@
 return {
   { "ray-x/lsp_signature.nvim" },
   { "onsails/lspkind.nvim" },
+  { "nvim-tree/nvim-web-devicons" },
 }
--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -1,6 +1,20 @@
 return {
   { "neovim/nvim-lspconfig" },
-  { "simrat39/rust-tools.nvim" },
+  {
+    "simrat39/rust-tools.nvim",
+    opts = {
+      server = {
+        settings = {
+          ["rust-analyzer"] = {
+            check = {
+              command = "clippy",
+              extraArgs = { "--all", "--", "-W", "clippy::all" },
+            },
+          },
+        },
+      },
+    },
+  },
   { "nvim-lua/plenary.nvim" },
   { "mfussenegger/nvim-dap" },
 }
