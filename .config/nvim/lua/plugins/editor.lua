345a5b47c39be1ab53a6b2e0972d4d9642b86f34
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-03-30 22:15:24 -0400

    Fixing autosave issues

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,7 +1,13 @@
 return {
   { "rmagatti/auto-session" },
-  -- { "Pocco81/auto-save.nvim", opts = { debounce_delay = 5000 } },
+  { "okuuva/auto-save.nvim" },
   { "numToStr/Comment.nvim" },
   { "kylechui/nvim-surround" },
   { "folke/neodev.nvim" },
+  {
+    "andrewferrier/wrapping.nvim",
+    config = function()
+      require("wrapping").setup()
+    end,
+  },
 }
