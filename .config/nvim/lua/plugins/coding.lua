1d6160c5e652c6e28f0d701e93df06496ab0e66d
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-03-31 18:23:13 -0400

    Adding chatgpt to nvim

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -5,4 +5,15 @@ return {
   {
     "sbdchd/neoformat",
   },
+  {
+    "jackMort/ChatGPT.nvim",
+    config = function()
+      require("chatgpt").setup({})
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
 }
