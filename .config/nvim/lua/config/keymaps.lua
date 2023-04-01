46aef84c09e902e54fe1806228889eff215be7c5
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-04-01 16:18:20 -0400

    Adding ability to pass between tmux panes and nvim panes

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_del_keymap("n", "<C-h>")
+vim.api.nvim_del_keymap("n", "<C-j>")
+vim.api.nvim_del_keymap("n", "<C-k>")
+vim.api.nvim_del_keymap("n", "<C-l>")
+
 require("which-key").register({
   ["<leader>"] = {
     d = {
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -17,4 +17,20 @@ return {
       require("wrapping").setup()
     end,
   },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      require("nvim-tmux-navigation").setup({
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        },
+      })
+    end,
+  },
 }
