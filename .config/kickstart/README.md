1b94606311f38e6c0a794e424e4fecb24039c246
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-04-11 11:44:54 -0400

    Adding neotest and updating keybinds

--- .config/kickstart/README.md
+++ .config/kickstart/README.md
@@ -1,3 +1,8 @@
 # Neovim configuration
 
-Inspired by and implemented from both [LazyVim](https://github.com/LazyVim/LazyVim/) and [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim). These are amazing projects that got me started on my Neovim journey. 
+Inspired by and implemented from both [LazyVim](https://github.com/LazyVim/LazyVim/) and [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim). These are amazing projects that got me started on my Neovim journey.
+
+### Required software
+
+- [fzf](https://github.com/junegunn/fzf)
+- [prettierd](https://github.com/fsouza/prettierd)
--- .config/kickstart/lua/util/init.lua
+++ .config/kickstart/lua/util/init.lua
@@ -110,4 +110,23 @@ function M.toggle_diagnostics()
   end
 end
 
+function M.toggle(option, silent, values)
+  if values then
+    if vim.opt_local[option]:get() == values[1] then
+      vim.opt_local[option] = values[2]
+    else
+      vim.opt_local[option] = values[1]
+    end
+    return Util.info("Set " .. option .. " to " .. vim.opt_local[option]:get(), { title = "Option" })
+  end
+  vim.opt_local[option] = not vim.opt_local[option]:get()
+  if not silent then
+    if vim.opt_local[option]:get() then
+      Util.info("Enabled " .. option, { title = "Option" })
+    else
+      Util.warn("Disabled " .. option, { title = "Option" })
+    end
+  end
+end
+
 return M
