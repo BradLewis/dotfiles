45d71b8019c497db1ea2b5872ede4df10a7b62d5
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-11-12 21:48:11 -0500

    add docker images for nvim-web-devicons

--- .config/nvim/lua/plugins/nvim-web-devicons.lua
+++ .config/nvim/lua/plugins/nvim-web-devicons.lua
@@ -1,13 +1,23 @@
 return {
   "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("nvim-web-devicons").set_icon({
+  config = {
+    override = {
       gql = {
         icon = "",
         color = "#e535ab",
         cterm_color = "199",
         name = "GraphQL",
       },
-    })
-  end,
+      dockerfile = {
+        icon = "",
+        color = "#1D63ED",
+        name = "Dockerfile",
+      },
+    },
+    override_by_filename = {
+      ["docker-compose.yaml"] = { icon = "", color = "#1D63ED", name = "Dockerfile" },
+      ["docker-compose.yml"] = { icon = "", color = "#1D63ED", name = "Dockerfile" },
+      [".dockerignore"] = { icon = "", color = "#1D63ED", name = "Dockerfile" },
+    },
+  },
 }
