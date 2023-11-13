1a3d40dad7d45e044be0bf15034a8016eff02952
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-11-12 20:29:22 -0500

    feat: add dap config

---
+++ .config/nvim-new/lua/plugins/lsp/nvim-dap.lua
@@ -0,0 +1,86 @@
+local M = {
+  "mfussenegger/nvim-dap",
+  event = "VeryLazy",
+
+  dependencies = {
+    {
+      "mfussenegger/nvim-dap-python",
+      config = function()
+        require("dap-python").setup()
+      end,
+    },
+    {
+      "rcarriga/nvim-dap-ui",
+
+      config = function()
+        require("dapui").setup()
+      end,
+    },
+    {
+      "mxsdev/nvim-dap-vscode-js",
+      config = function() end,
+    },
+  },
+}
+
+function M.config()
+  local dap, dapui = require("dap"), require("dapui")
+  dapui.setup({})
+  dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+  end
+  dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+  end
+  dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+  end
+
+  dap.adapters.lldb = {
+    type = "executable",
+    command = "/opt/homebrew/opt/llvm/bin/lldb-vscode",
+    name = "lldb",
+  }
+
+  -- configure the adapter for Rust Debugging
+  dap.configurations.rust = {
+    {
+      name = "Launch Debug",
+      type = "rt_lldb",
+      request = "launch",
+      program = function()
+        local default_file = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/" .. default_file, "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+      args = {},
+      initCommand = {},
+      runInTerminal = true,
+    },
+  }
+end
+
+require("which-key").register({
+  ["<leader>"] = {
+    d = {
+      name = "Debug",
+      b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle breakpoint" },
+      l = {
+        "<cmd>lua require'dap'.continue()<cr>",
+        "Launch debug session",
+      },
+      t = { "<cmd>lua require'dap'.terminate()<cr><cmd>lua require'dapui'.close()<cr>", "Terminate debug session" },
+      o = { "<cmd>lua require'dapui'.open()<cr>", "Open DAP UI" },
+      c = { "<cmd>lua require'dapui'.close()<cr>", "Close DAP UI" },
+      w = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle DAP UI" },
+    },
+  },
+  ["<F11>"] = { "<cmd>lua require'dap'.step_into()<cr>", "Step into" },
+  ["<F10>"] = { "<cmd>lua require'dap'.step_over()<cr>", "Step over" },
+  ["<F9>"] = { "<cmd>lua require'dap'.step_out()<cr>", "Step out" },
+  ["<F5>"] = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
+  ["<s-F5>"] = { "<cmd>lua require'dap'.disconnect({ terminateDebuggee = true })<cr>", "Disconnect" },
+})
+
+return M
--- .config/nvim-new/lua/plugins/telescope.lua
+++ .config/nvim-new/lua/plugins/telescope.lua
@@ -40,46 +40,12 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>sw", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-
-    -- local opts = {}
-    -- opts.desc = "Show LSP references"
-    -- keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-    --
-    -- opts.desc = "Go to declaration"
-    -- keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-    --
-    -- opts.desc = "Show LSP definitions"
-    -- keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-    --
-    -- opts.desc = "Show LSP implementations"
-    -- keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-    --
-    -- opts.desc = "Show LSP type definitions"
-    -- keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-    --
-    -- opts.desc = "See available code actions"
-    -- keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-    --
-    -- opts.desc = "Smart rename"
-    -- keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-    --
-    -- opts.desc = "Show buffer diagnostics"
-    -- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-    --
-    -- opts.desc = "Show line diagnostics"
-    -- keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-    --
-    -- opts.desc = "Go to previous diagnostic"
-    -- keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-    --
-    -- opts.desc = "Go to next diagnostic"
-    -- keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-    --
-    -- opts.desc = "Show documentation for what is under cursor"
-    -- keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-    --
-    -- opts.desc = "Restart LSP"
-    -- keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+    keymap.set(
+      "n",
+      "<leader>,",
+      "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
+      { desc = "Switch Buffer" }
+    )
   end,
   keys = {
     {
@@ -92,5 +58,8 @@ return {
       "<cmd>lua require('telescope.builtin').files({file_ignore_patterns = { '%.spec.ts' } })<cr>",
       desc = "Find files (ignoring test files)",
     },
+    { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
+    { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+    { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
   },
 }
