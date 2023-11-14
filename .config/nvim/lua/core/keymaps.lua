f619750ede4123b5dd68d7449f04d754ae7955c3
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-11-13 19:17:43 -0500

    add keymaps for shifting lines up and down

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -23,3 +23,10 @@ map.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -
 -- map.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 map.set("n", "<leader>qq", "<cmd>quitall<CR>", { desc = "Quit all" }) -- quit all
+
+map.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
