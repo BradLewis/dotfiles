bfed01e58038240855584f33b57c88d7b05acca7
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2023-12-13 15:04:03 -0500

    add fzf tmux session searcher

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -2,12 +2,14 @@ return {
   "navarasu/onedark.nvim",
   lazy = false,
   priority = 1000,
-  opts = {
-    style = "deep",
-  },
   config = function()
     require("onedark").setup({
       style = "deep",
+      diagnostics = {
+        darker = false,
+        undercurl = true,
+        background = true,
+      },
     })
     require("onedark").load()
   end,
