1d2a3ff5d4132075187e3b132d06ab20f4f2bf84
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2024-01-09 08:59:11 -0500

    fix none-ls autoformat on save

--- .config/nvim/lua/plugins/lsp/none-ls.lua
+++ .config/nvim/lua/plugins/lsp/none-ls.lua
@@ -63,13 +63,7 @@ return {
             group = augroup,
             buffer = bufnr,
             callback = function()
-              vim.lsp.buf.format({
-                filter = function(client)
-                  --  only use null-ls for formatting instead of lsp server
-                  return client.name == "null-ls"
-                end,
-                bufnr = bufnr,
-              })
+              vim.lsp.buf.format({ async = false })
             end,
           })
         end
