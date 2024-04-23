90ebfe8b23fdc7895990ba95cb3e5fe23dee5195
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2024-04-23 15:01:36 -0400

    remove nvim-tree and replace deprecated append in harpoon

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -44,7 +44,7 @@ return {
     end, { desc = "Open harpoon window" })
   end,
   keys = {
-    { "<leader>hm", "<cmd>lua require('harpoon'):list():append()<cr>", desc = "Mark file with harpoon" },
+    { "<leader>hm", "<cmd>lua require('harpoon'):list():add()<cr>", desc = "Mark file with harpoon" },
     { "<leader>hn", "<cmd>lua require('harpoon'):list():next()<cr>", desc = "Go to next harpoon mark" },
     { "<leader>hp", "<cmd>lua require('harpoon'):list():prev()<cr>", desc = "Go to previous harpoon mark" },
     { "<leader>ht", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Toggle quick menu" },
