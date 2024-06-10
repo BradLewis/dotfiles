bcdb4ff7514a2cbede2e8edf2de28c16480f1f8d
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2024-06-10 14:21:47 -0400

    fix deleting harpoon marks in the telescope window

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -9,30 +9,32 @@ return {
     local harpoon = require("harpoon")
     harpoon.setup({})
     local conf = require("telescope.config").values
-    local action_state = require("telescope.actions.state")
     local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
+      local finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+        return require("telescope.finders").new_table({
+          results = paths,
+        })
       end
 
       require("telescope.pickers")
         .new({}, {
           prompt_title = "Harpoon",
-          finder = require("telescope.finders").new_table({
-            results = file_paths,
-          }),
+          finder = finder(),
           previewer = conf.file_previewer({}),
           sorter = conf.generic_sorter({}),
-          attach_mappings = function(_, map)
-            map({ "n", "i" }, "<C-d>", function(prompt_bufnr)
-              local curr_picker = action_state.get_current_picker(prompt_bufnr)
+          attach_mappings = function(prompt_bufnr, map)
+            map("i", "<C-d>", function()
+              local state = require("telescope.actions.state")
+              local selected_entry = state.get_selected_entry()
+              local current_picker = state.get_current_picker(prompt_bufnr)
 
-              curr_picker:delete_selection(function(selection)
-                harpoon:list():removeAt(selection.index)
-              end)
+              table.remove(harpoon_files.items, selected_entry.index)
+              current_picker:refresh(finder())
             end)
-
             return true
           end,
         })
