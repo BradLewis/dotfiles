417e54faf8597ed99eb1e959843afbb167781772
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2024-07-11 13:30:32 -0400

    use relative imports for typescript

--- .config/nvim/lua/custom/plugins/typescript-tools.lua
+++ .config/nvim/lua/custom/plugins/typescript-tools.lua
@@ -2,6 +2,12 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
+    opts = {
+      settings = {
+        tsserver_file_preferences = {
+          importModuleSpecifierPreference = "relative",
+        },
+      },
+    },
   },
 }
